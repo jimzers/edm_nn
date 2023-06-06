39870ab72f1afdcdf9a1d6d44666c7c4ac4aa6aa
jimzers <jimzersml@gmail.com>
2023-06-06 09:06:47 -0700

    add wandb logging

--- src/callbacks.py
+++ src/callbacks.py
@@ -2,9 +2,11 @@
 Callbacks
 """
 
+import wandb
 import torch
-
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import Callback
+
 
 # class ActivationRecordingCallback(pl.Callback):
 #     """
@@ -18,8 +20,43 @@ import pytorch_lightning as pl
 #         self.activations.append(output.detach().clone())
 
 
+class LogPredictionsCallback(Callback):
 
-# make a pytorch hook to record activations
+    def __init__(self, wandb_logger, save_n=20, log_every=1):
+        """
+        Callback to log predictions to wandb.
+        Args:
+            wandb_logger:  wandb logger object
+            save_n: number of predictions to log
+            log_every: log predictions every XXX batches
+        """
+        super(LogPredictionsCallback, self).__init__()
+        self.wandb_logger = wandb_logger
+        self.save_n = save_n
+        self.log_every = log_every
+
+    def on_validation_batch_end(
+            self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+        """Called when the validation batch ends."""
+
+        # `outputs` comes from `LightningModule.validation_step`
+        # which corresponds to our model predictions in this case
+
+        # Let's log 20 sample image predictions from first batch
+        if batch_idx % self.log_every == 0:
+            n = min(len(batch), self.save_n)
+            x, y = batch
+            images = [img for img in x[:n]]
+            captions = [f'Ground Truth: {y_i} - Prediction: {y_pred}' for y_i, y_pred in zip(y[:n], outputs[:n])]
+
+            # Option 1: log images with `WandbLogger.log_image`
+            self.wandb_logger.log_image(key='sample_images', images=images, caption=captions)
+
+            # Option 2: log predictions as a Table
+            columns = ['image', 'ground truth', 'prediction']
+            data = [[wandb.Image(x_i), y_i, y_pred] for x_i, y_i, y_pred in list(zip(x[:n], y[:n], outputs[:n]))]
+            self.wandb_logger.log_table(key='sample_table', columns=columns, data=data)
+
 
 class ActivationRecordingHook:
     def __init__(self, module):
--- src/train_network.py
+++ src/train_network.py
@@ -8,12 +8,15 @@ python train_network --config experiments/train_mnist.yaml
 import argparse
 import yaml
 import datetime
+import pathlib
 
 from networks import MLPNet
 from datasets import load_dataset
-# from callbacks import ActivationRecordingHook
+from callbacks import ActivationRecordingHook, LogPredictionsCallback
 
-import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 if __name__ == "__main__":
     # Parse arguments
@@ -35,6 +38,15 @@ if __name__ == "__main__":
     dataset_params = config["data"]
     training_params = config["training"]
     logging_params = config["logging"]
+
+    # set up saving directories
+    save_dir = pathlib.Path(save_dir)
+    save_dir.mkdir(parents=True, exist_ok=True)
+    run_dir = save_dir / run_name
+    run_dir.mkdir(parents=True, exist_ok=True)
+
+    model_save_dir = run_dir / model_params["model_save_dir"]
+    model_save_path = model_save_dir / (run_name + ".ckpt")
 
     # load dataset
     train_loader, test_loader = load_dataset(
@@ -60,20 +72,32 @@ if __name__ == "__main__":
     else:
         raise NotImplementedError(f"Model {model_params['model_type']} not implemented.")
 
+    # set up logging
+    wandb_logger = WandbLogger(log_model="all", project=logging_params["wandb_project"], name=run_name)
+
     # load callbacks
     callbacks = []
     # if logging_params["record_activations"]:
     #     callbacks.append(ActivationRecordingCallback())
 
+    # save model checkpoints
+    if logging_params["save_checkpoints"]:
+        checkpoint_callback = ModelCheckpoint(monitor='val_accuracy', mode='max')
+
+    if logging_params["log_predictions"]:
+        callbacks.append(LogPredictionsCallback(wandb_logger))
+
     # train model
-    trainer = pl.Trainer(
+    trainer = Trainer(
         max_epochs=training_params["epochs"],
-        default_root_dir=model_params["model_save_dir"],
+        default_root_dir=model_save_dir,
         callbacks=callbacks,
+        logger=wandb_logger,
     )
+    # log gradients and model topology
+    wandb_logger.watch(model, log="all", log_freq=1)
+
     trainer.fit(model, train_loader, test_loader)
 
     # save model
-    trainer.save_checkpoint(model_params["model_save_dir"] + "/" + run_name + ".ckpt")
-
-
+    trainer.save_checkpoint(model_save_path)
